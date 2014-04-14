ca1224fe6a5ab022ecae4620a018d861c6aaea99
Rafael Barros <admin@wmfoi.com.br>
2014-04-14 11:26:55 -0300

    Independência de IDE; Alterações em test suite personalizado e arquivo de configuração do PHPUnit;

--- unit/MitiSuite.php
+++ unit/MitiSuite.php
@@ -1,6 +1,30 @@
 <?php
+/*essa classe e necessaria apenas para o NetBeans nao xiar.
+poderia ser passado apenas o diretorio para o phpunit*/
+
 class MitiSuite extends PHPUnit_Framework_TestSuite{
+	public static function suite(){
+		$suite=new MitiSuite();
+		
+		foreach(self::toRun() as $file){
+			$suite->addTestFile($file);
+		}
+		
+		return $suite;
+	}
+	
 	private static function toRun(){
-        return '/var/www/miti_modelo/unit/';
-    }
+		return self::rglob('*Test.php','/var/www/miti_modelo/unit/');
+	}
+	
+	private static function rglob($pattern='*',$path='',$flags=0){
+		$paths=glob($path.'*',GLOB_MARK|GLOB_ONLYDIR|GLOB_NOSORT) or array();
+		$files=glob($path.$pattern,$flags) or array();
+		
+		foreach($paths as $path){
+			$files=array_merge($files,self::rglob($pattern,$path,$flags));
+		}
+		
+		return $files;
+	}
 }
