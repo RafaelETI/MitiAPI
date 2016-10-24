19e906c547c410c0cf1228eb5961a6d09e691f0c
Rafael Barros <admin@rafaelbarros.eti.br>
2016-10-24 10:25:10 -0200

    \Miti\RIP::setHttp: criação.

--- tests/src/RIPTest.php
+++ tests/src/RIPTest.php
@@ -1,25 +1,26 @@
 <?php
 class RIPTest extends PHPUnit_Framework_TestCase{
-	private static $config;
-	
-	public static function setUpBeforeClass(){
-		global $config;
+    private static $config;
+
+    public static function setUpBeforeClass(){
+        global $config;
         self::$config = $config;
-	}
-	
-	public function testGetRequisitarJson(){
+    }
+
+    public function testGetRequisitarJson(){
         $rip = new \Miti\RIP(self::$config);
+        $rip->setHttp(CURL_HTTP_VERSION_1_1);
         $rip->setGet();
         $rip->setHeader('');
         $rip->setUrl('?json={0:true}');
-		$this->assertSame(true, $rip->requisitarJson()->validate);
-	}
-    
+        $this->assertSame(true, $rip->requisitarJson()->validate);
+    }
+
     public function testPostRequisitarJson(){
         $rip = new \Miti\RIP(self::$config);
         $rip->setPost();
         $rip->setUrl('?json={0:true}');
         $rip->setPostFields([]);
-		$this->assertSame(true, $rip->requisitarJson()->validate);
-	}
+        $this->assertSame(true, $rip->requisitarJson()->validate);
+    }
 }
