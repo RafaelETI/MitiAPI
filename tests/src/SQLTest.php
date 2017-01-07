dbe6a17960a113792168e181c60a2b447b256384
Rafael Barros <admin@rafaelbarros.eti.br>
2017-01-07 17:33:58 -0200

    orm -> sql

--- tests/src/SQLTest.php
+++ tests/src/SQLTest.php
@@ -4,18 +4,18 @@ use PHPUnit\Framework\TestCase;
 class SQLTest extends TestCase
 {
     private static $config;
-    private static $ormCategoria;
-    private static $ormMemoria;
-    private static $ormStatus;
+    private static $sqlCategoria;
+    private static $sqlMemoria;
+    private static $sqlStatus;
 
     public static function setUpBeforeClass()
     {
         global $config;
         self::$config = $config;
 
-        self::$ormCategoria = new \Miti\SQL($config, 'categoria', 'c');
-        self::$ormMemoria = new \Miti\SQL($config, 'memoria', 'm');
-        self::$ormStatus = new \Miti\SQL($config, 'status', 's');
+        self::$sqlCategoria = new \Miti\SQL($config, 'categoria', 'c');
+        self::$sqlMemoria = new \Miti\SQL($config, 'memoria', 'm');
+        self::$sqlStatus = new \Miti\SQL($config, 'status', 's');
     }
 
     public function testBanco()
@@ -27,30 +27,30 @@ class SQLTest extends TestCase
     public function testGetTipos()
     {
         $tipos = ['id' => 'float', 'nome' => 'string', 'status' => 'string'];
-        $this->assertSame($tipos, self::$ormCategoria->getTipos());
+        $this->assertSame($tipos, self::$sqlCategoria->getTipos());
     }
 
     public function testGetAnulaveis()
     {
         $anulaveis = ['id' => false, 'nome' => false, 'status' => true];
-        $this->assertSame($anulaveis, self::$ormCategoria->getAnulaveis());
+        $this->assertSame($anulaveis, self::$sqlCategoria->getAnulaveis());
     }
 
     public function testGetTamanhos()
     {
         $tamanhos = ['id' => 3, 'nome' => 30, 'status' => 1];
-        $this->assertSame($tamanhos, self::$ormCategoria->getTamanhos());
+        $this->assertSame($tamanhos, self::$sqlCategoria->getTamanhos());
     }
 
     public function testGetPk()
     {
-        $this->assertSame('id', self::$ormCategoria->getPk());
+        $this->assertSame('id', self::$sqlCategoria->getPk());
     }
 
     public function testValidarVazio()
     {
         $this->setExpectedException('UnexpectedValueException', "Valor vazio para o campo 'id'");
-        self::$ormCategoria->criar(['id' => '']);
+        self::$sqlCategoria->criar(['id' => '']);
     }
 
     public function testValidarExcessoDeCaracteres()
@@ -58,33 +58,33 @@ class SQLTest extends TestCase
         $mensagem = "Limite de caractéres excedido para o campo 'id'";
         $this->setExpectedException('UnexpectedValueException', $mensagem);
 
-        self::$ormCategoria->criar(['id' => 1000]);
+        self::$sqlCategoria->criar(['id' => 1000]);
     }
 
     public function testCriar()
     {
-        self::$ormCategoria->criar(['id' => 4, 'nome' => 'Teste', 'status' => 'c']);
-        self::$ormCategoria->criar(['id' => 5, 'nome' => 'Teste 2', 'status' => '']);
+        self::$sqlCategoria->criar(['id' => 4, 'nome' => 'Teste', 'status' => 'c']);
+        self::$sqlCategoria->criar(['id' => 5, 'nome' => 'Teste 2', 'status' => '']);
     }
 
     public function testAtualizar()
     {
-        self::$ormCategoria->filtrar('c', 'id', '=', '5')->atualizar(['status' => 'c']);
+        self::$sqlCategoria->filtrar('c', 'id', '=', '5')->atualizar(['status' => 'c']);
     }
 
     public function testDeletar()
     {
-        self::$ormCategoria->zerar();
+        self::$sqlCategoria->zerar();
 
-        self::$ormCategoria->filtrar('c', 'status', '=', 'c')->deletar();
+        self::$sqlCategoria->filtrar('c', 'status', '=', 'c')->deletar();
 
-        $quantidade = self::$ormCategoria->selecionar('c', 'id')->ler()->quantificar();
+        $quantidade = self::$sqlCategoria->selecionar('c', 'id')->ler()->quantificar();
         $this->assertSame(0, $quantidade);
     }
 
     public function testJuntar()
     {
-        $m = self::$ormMemoria
+        $m = self::$sqlMemoria
             ->selecionar('m', 'id')
             ->selecionar('s', 'descricao', 'des')
             ->juntar('categoria', 'c', 'm', 'categoria', 'c', 'id')
@@ -99,9 +99,9 @@ class SQLTest extends TestCase
 
     public function testJuntarEsquerda()
     {
-        self::$ormCategoria->zerar();
+        self::$sqlCategoria->zerar();
 
-        $c = self::$ormCategoria
+        $c = self::$sqlCategoria
             ->selecionar('c', 'nome')
             ->juntarEsquerda('status', 's', 'c', 'status', 's', 'id')
             ->filtrar('c', 'id', '=', '3')
@@ -114,9 +114,9 @@ class SQLTest extends TestCase
 
     public function testJuntarDireita()
     {
-        self::$ormStatus->zerar();
+        self::$sqlStatus->zerar();
 
-        $s = self::$ormStatus
+        $s = self::$sqlStatus
             ->selecionar('c', 'nome', 'c_nome')
             ->juntarDireita('categoria', 'c', 's', 'id', 'c', 'status')
             ->filtrar('c', 'id', '=', '3')
@@ -129,9 +129,9 @@ class SQLTest extends TestCase
 
     public function testEFiltrar()
     {
-        self::$ormMemoria->zerar();
+        self::$sqlMemoria->zerar();
 
-        $m = self::$ormMemoria
+        $m = self::$sqlMemoria
             ->selecionar('m', 'id')
             ->filtrar('m', 'categoria', '=', '1')
             ->eFiltrar('m', 'descricao', '=', 'Peaceful Warrior')
@@ -144,9 +144,9 @@ class SQLTest extends TestCase
 
     public function testOuFiltrar()
     {
-        self::$ormMemoria->zerar();
+        self::$sqlMemoria->zerar();
 
-        $m = self::$ormMemoria
+        $m = self::$sqlMemoria
             ->selecionar('m', 'id')
             ->filtrar('m', 'id', '=', '1')
             ->ouFiltrar('m', 'id', '=', '2')
@@ -159,16 +159,16 @@ class SQLTest extends TestCase
 
     public function testTratarLeitura()
     {
-        self::$ormCategoria->zerar();
-        $c = self::$ormCategoria->selecionar('c', 'id')->filtrar('c', 'nome', 'like', 'ilm')->ler()->quantificar();
+        self::$sqlCategoria->zerar();
+        $c = self::$sqlCategoria->selecionar('c', 'id')->filtrar('c', 'nome', 'like', 'ilm')->ler()->quantificar();
         $this->assertSame(1, $c);
     }
 
     public function testOrdenar()
     {
-        self::$ormMemoria->zerar();
+        self::$sqlMemoria->zerar();
 
-        $m = self::$ormMemoria
+        $m = self::$sqlMemoria
             ->selecionar('m', 'descricao')
             ->ordenar('m', 'categoria', 'asc')
             ->ordenar('m', 'descricao', 'desc')
@@ -181,14 +181,14 @@ class SQLTest extends TestCase
 
     public function testOrdenarAleatoriamente()
     {
-        self::$ormMemoria->zerar();
-        self::$ormMemoria->selecionar('m', 'id')->ordenarAleatoriamente();
+        self::$sqlMemoria->zerar();
+        self::$sqlMemoria->selecionar('m', 'id')->ordenarAleatoriamente();
 
         $resultado = false;
-        $controle = self::$ormMemoria->ler()->vetorizar();
+        $controle = self::$sqlMemoria->ler()->vetorizar();
 
         for ($i = 1; $i <= 10; $i++) {
-            $m = self::$ormMemoria->ler()->vetorizar();
+            $m = self::$sqlMemoria->ler()->vetorizar();
 
             if ($m['id'] != $controle['id']) {
                 $resultado = true;
@@ -201,9 +201,9 @@ class SQLTest extends TestCase
 
     public function testAgrupar()
     {
-        self::$ormMemoria->zerar();
+        self::$sqlMemoria->zerar();
 
-        $m = self::$ormMemoria
+        $m = self::$sqlMemoria
             ->selecionar('s', 'prioridade')
             ->juntar('categoria', 'c', 'c', 'id', 'm', 'categoria')
             ->juntar('status', 's', 's', 'id', 'c', 'status')
@@ -216,29 +216,29 @@ class SQLTest extends TestCase
 
     public function testLimitarZero()
     {
-        self::$ormMemoria->zerar();
-        $quantidade = self::$ormMemoria->selecionar('m', 'id')->limitar(0)->ler()->quantificar();
+        self::$sqlMemoria->zerar();
+        $quantidade = self::$sqlMemoria->selecionar('m', 'id')->limitar(0)->ler()->quantificar();
         $this->assertSame(3, $quantidade);
     }
 
     public function testLimitar()
     {
-        self::$ormMemoria->zerar();
-        $quantidade = self::$ormMemoria->selecionar('m', 'id')->limitar(1, 2)->ler()->quantificar();
+        self::$sqlMemoria->zerar();
+        $quantidade = self::$sqlMemoria->selecionar('m', 'id')->limitar(1, 2)->ler()->quantificar();
         $this->assertSame(1, $quantidade);
     }
 
     public function testZerar()
     {
-        self::$ormMemoria->zerar()->limitar(1)->zerar();
-        $quantidade = self::$ormMemoria->selecionar('m', 'id')->ler()->quantificar();
+        self::$sqlMemoria->zerar()->limitar(1)->zerar();
+        $quantidade = self::$sqlMemoria->selecionar('m', 'id')->ler()->quantificar();
         $this->assertSame(3, $quantidade);
     }
 
     public static function tearDownAfterClass()
     {
-        self::$ormCategoria->getBanco()->rebobinar();
-        self::$ormMemoria->getBanco()->rebobinar();
-        self::$ormStatus->getBanco()->rebobinar();
+        self::$sqlCategoria->getBanco()->rebobinar();
+        self::$sqlMemoria->getBanco()->rebobinar();
+        self::$sqlStatus->getBanco()->rebobinar();
     }
 }
